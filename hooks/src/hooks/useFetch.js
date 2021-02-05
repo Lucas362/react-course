3bce5bc3ba9ba598dcbc434648312af870c15ef3
Lucas362 <lucasvitorifg@gmail.com>
2021-02-04 23:09:53 -0300

    hooks

---
+++ hooks/src/hooks/useFetch.js
@@ -0,0 +1,34 @@
+import {useState, useEffect} from 'react';
+
+export const useFetch = (url, method = 'get') => {
+    const [response, setResponse] = useState({
+        data: null,
+        loading: true
+    });
+
+    useEffect(function() {
+        // fetch(url, { method })
+        // .then(resp => resp.json())
+        // .then(json => setResponse({
+        //     data: json,
+        //     loading: false
+        // }));
+
+        setResponse({
+            data: [
+                {
+                "nome": "Acre",
+                "sigla": "AC"
+                },
+                {
+                "nome": "Alagoas",
+                "sigla": "AL"
+                },
+            ],
+            loading: false
+        });
+    }, [url, method])
+
+
+    return response
+}
